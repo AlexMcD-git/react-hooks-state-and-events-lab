48531b1818c7e578f4a716cf2cc9cfbf5a93d85c
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-04-28 11:13:52 -0400

    commit tree 260ed26dd071d2465f1afc5a95ff1e2fdc3bd528

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [cartClass, setCartClass] = useState("")
+  function handlerCart(){
+    setCartClass(cartClass=>cartClass?"":"in-cart")
+  }
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handlerCart}>{cartClass?"In Cart":"Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [catFilter, setCatFilter] = useState("All")
+  function handlerFilter(event){
+    console.log(event.target.value)
+    setCatFilter(event.target.value)
+    console.log(catFilter)
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handlerFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -14,7 +21,7 @@ function ShoppingList({ items }) {
       </div>
       <ul className="Items">
         {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+          catFilter==="All"?<Item key={item.id} name={item.name} category={item.category} />:catFilter===item.category?<Item key={item.id} name={item.name} category={item.category} />: null
         ))}
       </ul>
     </div>
